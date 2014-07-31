078b00c5a6c2df5b04453e68dbeeafc94125e56a
Kyle Rich <kyle.rich@dealertrack.com>
2014-07-31 15:28:15 -0600

    Add comments to installer script

--- install.sh
+++ install.sh
@@ -6,12 +6,17 @@ fi
 mkdir -p ~/.backup
 for file in home/*; do
     if [ -f $file ]; then
+        # Archive old files, hard link new ones in
         mv -f ~/${file##*/} ~/.backup/
         ln $file ~/${file##*/}
+
     elif [ -d $file ]; then
+        # Archive old folders, symlink new ones in
         mv -f ~/${file##*/} ~/.backup/${file##*/} 
         ln -s $(pwd)/$file ~/${file##*/}
+
         if [ ! -h ~/${file##*/} ]; then
+            # If the old folder wasn't already a symlink, copy its contents back
             cp -npR ~/.backup/${file##*/}/* ~/${file##*/}/
         fi
     fi
