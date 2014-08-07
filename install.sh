7d1c57e7edd5b0c348890b5023ec3b29cd65b5cd
Kyle Rich <kyle.rich@dealertrack.com>
2014-08-07 12:03:58 -0600

    Attempt to fix install script

--- install.sh
+++ install.sh
@@ -1,36 +1,43 @@
 if [ ! -d home ]; then
     echo "No home folder to install"
-    return
+    exit
 fi
 
 # Setup vars
 CURR_SHELL=$(ps ch -o command $$ | grep -iv command)
 
-# Set options
-if [ $CURR_SHELL = bash ]; then
-    shopt -u dotglob
-else if [ $CURR_SHELL = zsh ]; then
-    setopt no_dot_glob
-fi
-
-mkdir -p ~/.backup
-for file in home/*; do
-    # Archive old version of file / folder
-    mv -f ~/${file##*/} ~/.backup/
-
-    if [ -f $file ]; then
-        # Hard link new config files in
-        ln $file ~/${file##*/}
-
-    elif [ -d $file ]; then
-        # Symlink new config dirs in
-        ln -s $(pwd)/$file ~/${file##*/}
-
-        if [ ! -h ~/.backup/${file##*/} ]; then
-            # If the old directory wasn't already a symlink, copy its contents back
-            cp -npR ~/.backup/${file##*/}/* ~/${file##*/}/
-        fi
+(
+    # Set options
+    if [ $CURR_SHELL = zsh ]; then
+        setopt dot_glob
+        setopt null_glob
+    else
+        shopt -s dotglob
+        shopt -s nullglob
     fi
-done
+
+    mkdir -p ~/.backup
+    for file in home/*; do
+        # Archive old version of file / folder
+        if [ -a ~/${file##*/} ]; then
+            mv -f ~/${file##*/} ~/.backup/
+        fi
+
+        if [ -f $file ]; then
+            # Hard link new config files in
+            ln $file ~/${file##*/}
+
+        elif [ -d $file ]; then
+            # Symlink new config dirs in
+            ln -s $(pwd)/$file ~/${file##*/}
+
+            if [ ! -h ~/.backup/${file##*/} ]; then
+                # If the old directory wasn't already a symlink, copy its contents back
+                cp -npR ~/.backup/${file##*/}/* ~/${file##*/}/
+            fi
+        fi
+    done
+)
 
 source home/.include/.source
+
