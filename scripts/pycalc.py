b8fe0f90a0064b7a3e4d990efad8fd4e1b1ed554
Kyle Rich <kyle.rich@dealertrack.com>
2015-06-08 11:25:16 -0600

    Remove autocli from PyCalc script.
    I should never have pasted the code in, but I also didn't want to
    have to pip install my autocli decorator just to install my dotfiles
    on another computer.  So I just quit depending on autocli, and am
    now using argparse for this case, quite contentedly.

--- scripts/pycalc.py
+++ scripts/pycalc.py
@@ -9,117 +9,51 @@ from decimal import Decimal
 from pprint import pprint as pp
 
 import importlib
-from inspect import getargspec
 import argparse
 
 
-class Argument:
-    def __init__(self, name, nick, default):
-        self.name = name
-        self.nick = nick
-        self.default = default
-        self.description = ''
+class PyCalc:
+    def __init__(self):
+        self.options, self.query = self.parse_args()
+        self.calc()
 
-    def get_args(self):
-        if self.default is not None:
-            return '-' + self.nick, '--' + self.name
-        else:
-            return self.name,
-
-    def get_kwargs(self):
-        kwargs = {'help': self.description}
-
-        if self.default is not None:
-            kwargs['default'] = self.default
-
-            if isinstance(self.default, bool):
-                kwargs['action'] = 'store_true'
-            else:
-                kwargs.update({'nargs': '?', 'const': True, 'type': type(self.default)})
-
-        return kwargs
-
-
-def autocli(func):
-    def _autocli(*args, **kwargs):
-        args = list(args)
-        nicks = {}
-        signature_args = getargspec(func)[0]
-
-        # Generate one-letter nicknames for each argument requested
-        for arg in signature_args:
-            if len(arg) <= 1:
-                continue
-
-            for letter in arg:
-                if letter.lower() not in nicks:
-                    nicks[letter.lower()] = arg
-                    break
-
-                if letter.upper() not in nicks:
-                    nicks[letter.upper()] = arg
-                    break
-
-        # Parse the parameters passed in with argparse
-        docs = (func.__doc__ if func.__doc__ else '').split('\n')
-        requested = getargspec(func)[0] or []
-        defaults = getargspec(func)[3] or []
-        reverse_nicks = {v: k for k, v in nicks.items()}
-        arguments = {}
-
-        name, _, _ = sys.argv[0].rpartition('.')
-        description = ''
-        if not docs[0].startswith(' '):
-            description = '\n'.join(filter(lambda x: ':' not in x, docs))
-            docs = filter(lambda x: ':' in x, docs)
-
-        for i, arg in enumerate(requested):
-            arguments[arg] = Argument(
-                arg,
-                reverse_nicks.get(arg, ''),
-                defaults[i - len(requested)] if i >= len(requested) - len(defaults) else None
-            )
-        for line in docs:
-            argname, _, help_text = line.strip().partition(':')
-            if argname and help_text:
-                arguments[argname].description = help_text.strip()
-
+    def parse_args(self):
         parser = argparse.ArgumentParser(
-            description=description,
-            formatter_class=argparse.ArgumentDefaultsHelpFormatter,
-            argument_default=argparse.SUPPRESS,
+            description=self.calc.__doc__,
         )
+        parser.add_argument('-c', '--collections', action='store_true')
+        parser.add_argument('-C', '--csv', action='store_true')
+        parser.add_argument('-d', '--datetime', action='store_true')
+        parser.add_argument('-r', '--re', action='store_true')
+        parser.add_argument('-s', '--shutil', action='store_true')
+        parser.add_argument('-z', '--zlib', action='store_true')
+        options, query = parser.parse_known_args()
 
-        for arg in arguments.values():
-            parser.add_argument(*arg.get_args(), **arg.get_kwargs())
-        parsed_args, unknown = parser.parse_known_args()
-        sys.argv = sys.argv[0:1] + unknown
+        if not query:
+            parser.print_help()
+            sys.exit(0)
+        else:
+            return vars(options), ''.join(query)
 
-        kwargs.update({k: v for k, v in vars(parsed_args).items() if v != arguments[k].default})
+    def calc(self):
+        """Python Calculator - Comprehensive command line interface to python expressions.
 
-        return func(*args, **kwargs)
+        Batteries included!  Imports os, sys, math.*, random.*, Decimal, and pprint as pp.
 
-    return _autocli
+        Also includes additional modules based on the following switches.
+        """
 
+        for k, v in self.options.items():
+            if v:
+                globals().update(vars(importlib.import_module(k)))
 
-@autocli
-def main(collections=False, csv=False, datetime=False, re=False, shutil=False, zlib=False):
-    """Python Calculator - Comprehensive command line interface to python expressions.
+        try:
+            result = eval(self.query, globals(), {})
+            if result:
+                print(result)
+        except Exception:
+            exec(self.query, globals(), {})
 
-    Batteries included!  Imports os, sys, math.*, random.*, Decimal, and pprint as pp.
-
-    Also includes additional modules based on the following switches.
-    """
-    for k, v in locals().items():
-        if v:
-            globals().update(importlib.import_module(k).__dict__)
-
-    try:
-        result = eval(''.join(sys.argv[1:]), globals(), {})
-        if result:
-            print(result)
-    except Exception:
-        exec(''.join(sys.argv[1:]), globals(), {})
 
 if __name__ == '__main__':
-    main()
+    PyCalc()
