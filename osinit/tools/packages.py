70b063647a3fc0676848cfd0bf36afb135a6a7cb
Kyle Rich <yutakafrog@gmail.com>
2016-02-04 22:51:07 -0700

    Add package installer

--- osinit/tools/packages.py
+++ osinit/tools/packages.py
@@ -1,7 +1,55 @@
+#! /usr/bin/env python
+
+from subprocess import check_output
+
+LINUX = "Linux"
+MAC = "Darwin"
+
+PACKAGES = {
+    'Silver Searcher': ('silversearcher-ag', 'ag'),
+    'ditaa': 'ditaa',
+    'dtrx': 'dtrx',
+    'git': 'git',
+    'graphviz': 'graphviz',
+    'htop': 'htop',
+    'httpie': 'httpie',
+    'iftop': 'iftop',
+    'invoke': 'invoke',
+    'inxi': 'inxi',
+    'irssi': 'irssi',
+    'm4': 'm4',
+    'ncdu': 'ncdu',
+    'pwgen': 'pwgen',
+    'ranger': 'ranger',
+    'tree': 'tree',
+    'w3m': 'w3m',
+    'wavemon': 'wavemon',
+    'xclip': ('xclip', None),
+    'z': 'z'
+}
 
 
-sudo apt-get update
-sudo apt-get upgrade
+def get_packages(system):
+    return 'xclip'
+    return ' '.join(filter(None, (v if not isinstance(v, tuple) else v[0 if system == LINUX else 1] for k, v in PACKAGES)))
 
-apt-get install -y silversearcher-ag ditaa dtrx git graphviz htop httpie iftop invoke inxi irssi m4 ncdu pwgen ranger \
-    tree vim w3m wavemon xclip z
+
+def run(command):
+    return check_output(command.split())
+
+
+def main():
+    system = run("uname -s")
+
+    if system == "Darwin":
+        pass
+
+    elif system == "Linux":
+        run('sudo apt-get update')
+        run('sudo apt-get upgrade')
+
+        run('sudo apt-get install -y {}'.format(get_packages(system)))
+
+
+if __name__ == '__main__':
+    main()
