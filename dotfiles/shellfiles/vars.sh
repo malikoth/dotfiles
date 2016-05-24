4a1aaaac9459471379b8b2504d832b4b2c58cb21
Kyle Rich <yutakafrog@gmail.com>
2016-05-24 10:25:03 -0600

    Put the pager back to less, I fixed the thing that was bugging me

--- dotfiles/shellfiles/vars.sh
+++ dotfiles/shellfiles/vars.sh
@@ -1,11 +1,11 @@
 # TODO: Make PATH specific to which shell or box I'm on
 export PATH="$HOME/.local/bin:$HOME/dev/misc/scripts:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/opt/X11/bin"
-export PAGER='vimpager'
+# export PAGER='vimpager'
 export EDITOR='vim'
 export VISUAL=$EDITOR
 export GREP_COLOR='01;34'
 export GREP_OPTIONS='--color=auto -i --exclude=*.pyc --exclude-dir=.git'
 export CLICOLOR='1'
 export LSCOLORS='GxFxCxDxBxegedabagaced'
-export LESS='-g -i -M -R -S -w -z-4'
+export LESS='-g -i -M -R -S -z-4'
 export PROJECT_HOME=$HOME/dev
