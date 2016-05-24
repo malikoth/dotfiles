3595e42bc48bfb9e7c15dd6c6479d22073bdaf47
Kyle Rich <yutakafrog@gmail.com>
2016-05-24 11:22:18 -0600

    Add build and run scripts, update Dockerfile

---
+++ dotfiles/tools/docker-run.bat
@@ -0,0 +1,1 @@
+docker run -it --rm -v d:/dev:/root/dev -v /var/run/docker.sock:/var/run/docker.sock malikoth/devenv
---
+++ dotfiles/tools/docker-run.sh
@@ -0,0 +1,1 @@
+docker run -it --rm -v ~/dev:/root/dev -v /var/run/docker.sock:/var/run/docker.sock malikoth/devenv
