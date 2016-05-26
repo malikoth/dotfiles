045ff522cf44106266dc2cfbdad0c947f3c681e2
Kyle Rich <yutakafrog@gmail.com>
2016-05-25 22:52:23 -0600

    Make grep work the same on Linux and Mac

--- dotfiles/shellfiles/vars.sh
+++ dotfiles/shellfiles/vars.sh
@@ -1,10 +1,9 @@
 # TODO: Make PATH specific to which shell or box I'm on
 export PATH="$HOME/.local/bin:$HOME/dev/misc/scripts:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/opt/X11/bin"
-# export PAGER='vimpager'
 export EDITOR='vim'
 export VISUAL=$EDITOR
 export GREP_COLOR='01;34'
-export GREP_OPTIONS='--color=auto -i --exclude=*.pyc --exclude-dir=.git'
+export GREP_COLORS="mt=$GREP_COLOR"
 export CLICOLOR='1'
 export LSCOLORS='GxFxCxDxBxegedabagaced'
 export LESS='-g -i -M -R -S -z-4'
