73b9d0245e26b8310c58f967be68093ef7261533
Kyle Rich <yutakafrog@gmail.com>
2016-08-02 12:43:16 -0600

    Finally switch to vi-style zsh line editing

--- shellfiles/function.sh
+++ shellfiles/function.sh
@@ -23,13 +23,3 @@ pyclean() {
     find ${*:-'.'} -type d -name "__pycache__" -delete
 }
 
-sudo-command-line() {
-    [[ -z $BUFFER ]] && zle up-history
-    if [[ $BUFFER == sudo\ * ]]; then
-        LBUFFER="${LBUFFER#sudo }"
-    else
-        LBUFFER="sudo $LBUFFER"
-    fi
-}
-zle -N sudo-command-line
-bindkey "\e\e" sudo-command-line
---
+++ shellfiles/zshrc.zsh
@@ -0,0 +1,20 @@
+bindkey -v
+
+bindkey '^P' up-history
+bindkey '^N' down-history
+bindkey '^w' backward-kill-word
+bindkey '^r' history-incremental-search-backward
+bindkey '^u' vi-change-whole-line
+bindkey 'jk' vi-cmd-mode
+
+sudo-command-line() {
+    [[ -z $BUFFER ]] && zle up-history
+    if [[ $BUFFER == sudo\ * ]]; then
+        LBUFFER="${LBUFFER#sudo }"
+    else
+        LBUFFER="sudo $LBUFFER"
+    fi
+}
+zle -N sudo-command-line
+bindkey '^s' sudo-command-line
+
