18a2267e9718562c2403482f0e54f16b7e9932fb
Kyle Rich <yutakafrog@gmail.com>
2016-08-02 13:06:52 -0600

    Add line start / end movement to vim and zsh

--- shellfiles/zshrc.zsh
+++ shellfiles/zshrc.zsh
@@ -4,6 +4,9 @@ bindkey '^P' up-history
 bindkey '^N' down-history
 bindkey '^w' backward-kill-word
 bindkey '^r' history-incremental-search-backward
+bindkey ',h' beginning-of-line
+bindkey ',l' end-of-line
+bindkey '^k' kill-line
 bindkey '^u' vi-change-whole-line
 bindkey 'jk' vi-cmd-mode
 
