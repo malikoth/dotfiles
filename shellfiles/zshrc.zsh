08f8d4c399908fa4ea910e9a2a23b8ba58f145d7
Kyle Rich <yutakafrog@gmail.com>
2017-03-01 12:24:06 -0700

    Make vivisual line editing work better

--- shellfiles/zshrc.zsh
+++ shellfiles/zshrc.zsh
@@ -3,12 +3,15 @@ bindkey -v
 bindkey '^P' up-history
 bindkey '^N' down-history
 bindkey '^w' backward-kill-word
-bindkey '^r' history-incremental-search-backward
+bindkey '^h' history-incremental-search-backward
 bindkey ',h' beginning-of-line
 bindkey ',l' end-of-line
 bindkey '^k' kill-line
 bindkey '^u' vi-change-whole-line
 bindkey 'jk' vi-cmd-mode
+bindkey -v "$key_info[Control]X$key_info[Control]E" edit-command-line
+bindkey -a '!' edit-command-line
+bindkey -a 'v' visual-mode
 
 sudo-command-line() {
     [[ -z $BUFFER ]] && zle up-history
