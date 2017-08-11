4511aa595b94c8f7d98b602ffc461f7d2d7f8e65
Kyle Rich <yutakafrog@gmail.com>
2017-08-10 18:07:31 -0600

    Make installer links work maybe?

--- tools/install.sh
+++ tools/install.sh
@@ -3,9 +3,9 @@
 DOTFILES=${HOME}/.dotfiles
 ZDOTDIR=${HOME}/.config/zsh/
 
-ln -si ${DOTFILES}/.config/ ${HOME}/.config/
-ln -si ${DOTFILES}/.ssh/ ${HOME}/.ssh/
-ln -si ${DOTFILES}/.vim/ ${HOME}/.vim/
+ln -si ${DOTFILES}/.config/ ${HOME}/.config
+ln -si ${DOTFILES}/.ssh/ ${HOME}/.ssh
+ln -si ${DOTFILES}/.vim/ ${HOME}/.vim
 ln -si ${DOTFILES}/.zshenv ${HOME}/.zshenv
 
 git clone --recursive https://github.com/sorin-ionescu/prezto.git ${ZDOTDIR}/.zprezto
