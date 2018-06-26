c497eed49ccb223d2e7d0cb7ff15ca01c30dcc82
Kyle Rich <yutakafrog@gmail.com>
2018-06-26 16:31:44 -0600

    Add csv alias as a shortcut to column command, reorganize aliases slightly

--- .config/shell/alias.sh
+++ .config/shell/alias.sh
@@ -8,7 +8,6 @@ alias hl='history P'
 alias tree='tree -shC --du P'
 alias z='fasd_cd'
 alias zz='fasd_cd -i $(fc -nl -1 | awk '"'"'{print $NF}'"'"')'
-alias gu='cd "$(git rev-parse --show-toplevel)"'
 
 # System Management
 alias apt-up='sudo apt update && sudo apt upgrade -y'
@@ -24,29 +23,31 @@ alias pip-up='pip list | cut -d " " -f 1 | xargs pip install --upgrade'
 alias yau="yadm add -u ${HOME}"
 
 # Git
-alias gcom='git checkout master'
-alias gnp='git --no-pager'
-alias gwdnp='git --no-pager diff'
 alias gbc='git branch --merged | egrep -v "(^\*|master|dev)" | xargs git branch -d'  # git branch clean
 alias gcb='git checkout -b'
+alias gcom='git checkout master'
+alias gnp='git --no-pager'
 alias grph='git rev-parse HEAD'
+alias gu='cd "$(git rev-parse --show-toplevel)"'
+alias gwdnp='git --no-pager diff'
 
 # Miscellaneous
 alias -- -='cd -'
+alias csv='column -t -n -s,'
 alias dkhs='docker search'
-alias i='invoke'
-alias rl="source ${ZDOTDIR}/.zshrc"  # reload shell config
-alias rlc='eval $(dircolors ~/.config/dircolors.txt)'
-alias ra='ranger'
 alias ff='for font in `figlist | head -$(echo $(figlist | grep -n "Figlet control files" | cut -d : -f 1) - 1 | bc) | tail -n +4`; do echo $font; figlet -f $font Hello Kyle; done'
-alias pat='pygmentize -O style=monokai -f 16m -g'
-alias ws='python3 -m http.server'
 alias grep='grep -i --color=auto'
-alias rg="rg --ignore-file ${HOME}/.config/ripgrep/ignore"
-alias updatedb='sudo /usr/libexec/locate.updatedb'
+alias i='invoke'
+alias pat='pygmentize -O style=monokai -f 16m -g'
 alias path='echo $PATH | tr ":" "\n" | nl'
 alias plz='sudo $(fc -ln -1)'
+alias ra='ranger'
+alias rl="source ${ZDOTDIR}/.zshrc"  # reload shell config
+alias rlc='eval $(dircolors ~/.config/dircolors.txt)'
+alias rg="rg --ignore-file ${HOME}/.config/ripgrep/ignore"
+alias updatedb='sudo /usr/libexec/locate.updatedb'
 alias v='vagrant'
+alias ws='python3 -m http.server'
 
 # Zsh -g aliases
 alias -g G='| grep'
