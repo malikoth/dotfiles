c88c486b4f79789c57e412680fca67bd3648c8a6
Kyle Rich <yutakafrog@gmail.com>
2018-07-24 11:17:45 -0600

    Make the keygen work even first time with a call to mkdir -p

--- .config/shell/function.sh
+++ .config/shell/function.sh
@@ -33,5 +33,6 @@ keygen() {
     # $1 is the name of the server or service the key is for
     # $2 is an email address to associate with the key, and/or comment about the key
     # Both are concatenated along with the date to form the whole comment
+    mkdir -p ~/.ssh/keys
     ssh-keygen -t ed25519 -a 100 -f ~/.ssh/keys/$1 -C "$1 $2 $(date +%Y-%m-%d)"
 }
