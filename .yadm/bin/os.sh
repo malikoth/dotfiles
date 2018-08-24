43658293ffc98d2757f98d2dd8486c8da08f3a11
Kyle Rich <yutakafrog@gmail.com>
2018-08-24 15:30:47 -0600

    Break bootstrap into modules

---
+++ .yadm/bin/os.sh
@@ -0,0 +1,57 @@
+#! /usr/bin/env sh
+
+# This script assumes the pre-installation of curl and sed
+# This will install packages and do other OS-specific stuff
+
+
+if [ "$(id -u)" != "0" ] && command -v sudo; then
+    SUDO=sudo
+fi
+
+
+# MacOS
+#######
+
+# Install Homebrew
+if [ "$(uname -s)" = "Darwin" ] && ! command -v brew >/dev/null 2>&1; then
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+
+# Install packages via Homebrew
+if command -v brew >/dev/null 2>&1; then
+    brew bundle --file=${HOME}/.config/packages/Brewfile
+fi
+
+
+install_ripgrep() {
+    RIPGREP_VERSION=0.8.1
+    RIPGREP=ripgrep-${RIPGREP_VERSION}-x86_64-unknown-linux-musl
+    RIPGREP_OPT=${HOME}/.local/opt/ripgrep
+
+    rm -rf ${RIPGREP_OPT}
+    mkdir -p ${RIPGREP_OPT}
+    curl -fsSL https://github.com/BurntSushi/ripgrep/releases/download/${RIPGREP_VERSION}/${RIPGREP}.tar.gz | tar xzC ${RIPGREP_OPT} --strip-components=1 ${RIPGREP}
+    ln -sf ${RIPGREP_OPT}/rg ${HOME}/.local/bin/rg
+    mkdir -p ${HOME}/.local/share/man/man1
+    ln -sf ${RIPGREP_OPT}/rg.1 ${HOME}/.local/share/man/man1/rg.1
+    mandb >/dev/null 2>&1
+}
+
+
+# Debian-like
+#############
+
+if command -v apt >/dev/null 2>&1; then
+    $SUDO apt update && $SUDO apt install -y $(sed -e '/\s*#.*/d' ${HOME}/.config/packages/apt.txt)
+    install_ripgrep
+fi
+
+
+# Alpine
+########
+
+if command -v apk >/dev/null 2>&1; then
+    $SUDO apk add --no-cache $(sed -e '/\s*#.*/d' ${HOME}/.config/packages/apk.txt)
+    install_ripgrep
+fi
---
+++ .yadm/bin/python.sh
@@ -0,0 +1,30 @@
+#! /usr/bin/env sh
+
+# Install and setup Python, pip, and install system packages I like for Python
+
+
+if [ "$(id -u)" != "0" ] && command -v sudo; then
+    SUDO=sudo
+fi
+
+
+# TODO: Install pip iff not installed, upgrade iff not latest, install packages iff not installed, upgrade iff updates available
+if command -v python3.6 >/dev/null 2>&1; then
+    ln -s /usr/bin/python3.6 /usr/bin/python3
+fi
+
+
+if command -v python3 >/dev/null 2>&1; then
+    ln -s /usr/bin/python3 /usr/bin/python
+fi
+
+
+if command -v python >/dev/null 2>&1; then
+    if ! command -v pip >/dev/null 2>&1; then
+        curl -fsSL https://bootstrap.pypa.io/get-pip.py | $SUDO python
+    fi
+    $SUDO python -m pip install --upgrade --force-reinstall pip
+
+    # TODO: Use pipsi here
+    $SUDO pip install -r ${HOME}/.config/packages/pip.txt
+fi
