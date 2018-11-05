e4750ba8a2b5645f7915641a146ddc2134731294
Kyle Rich <yutakafrog@gmail.com>
2018-11-05 14:24:53 -0700

    Make ipython look in XDG_CONFIG_HOME for config files

--- .config/shell/xdg.sh
+++ .config/shell/xdg.sh
@@ -4,6 +4,9 @@ export GNUPGHOME=${XDG_CONFIG_HOME}/gnupg
 # httpie
 export HTTPIE_CONFIG_DIR="${XDG_CONFIG_HOME}/httpie"
 
+# ipython
+export IPYTHONDIR="${XDG_CONFIG_HOME}/ipython"
+
 # less
 export LESSHISTFILE="${XDG_CONFIG_HOME}/less/history"
 export LESSKEY="${XDG_CONFIG_HOME}/less/keys"
