d7884602283a3734eb52d5409724fa112151a46d
Kyle Rich <krich@hirevue.com>
2018-11-16 21:07:59 +0000

    Add more imports, including all from HV

--- .config/ipython/profile_default/startup/imports.py
+++ .config/ipython/profile_default/startup/imports.py
@@ -1,11 +1,20 @@
-from __future__ import division, print_function, unicode_literals
-
+import collections
+import contextlib
+import csv
 from datetime import date, datetime, timedelta
 import functools
 import hashlib
 import itertools
+import io
+import json
 import math
 import os
 import random
+import re
 import sys
 import time
+
+try:
+    from all import *
+except ImportError:
+    pass
