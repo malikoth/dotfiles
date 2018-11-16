3b88a55c8e2e03899665707ad8c1b2ffe0eb3ea4
Kyle Rich <yutakafrog@gmail.com>
2018-11-16 13:53:54 -0700

    Auto import common modules in ipython

---
+++ .config/ipython/profile_default/startup/imports.py
@@ -0,0 +1,11 @@
+from __future__ import division, print_function, unicode_literals
+
+from datetime import date, datetime, timedelta
+import functools
+import hashlib
+import itertools
+import math
+import os
+import random
+import sys
+import time
