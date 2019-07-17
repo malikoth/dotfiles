ed6110788a3ab6dacc0d153b016decad80e31d80
Kyle Rich <yutakafrog@gmail.com>
2019-07-17 12:47:45 -0600

    Update keys, add history function, and "git checkout date" alias

--- .config/shell/functions.sh
+++ .config/shell/functions.sh
@@ -29,10 +29,18 @@ gstat() {
     git status --porcelain | awk "\$1 == \"$1\" { print \$2 }"
 }
 
+gcod() {
+    git checkout $(git rev-list -1 --before="$1" $2)
+}
+
 keygen() {
     # $1 is the name of the server or service the key is for
     # $2 is an email address to associate with the key, and/or comment about the key
     # Both are concatenated along with the date to form the whole comment
     mkdir -p ~/.ssh/keys
     ssh-keygen -t ed25519 -a 100 -f ~/.ssh/keys/$1 -C "$1 $2 $(date +%Y-%m-%d)"
+}
+
+h() {
+    fc -lim "*$@*" 1
 }
