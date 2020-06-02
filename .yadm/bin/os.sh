f606df95566d8a6959f9e71ace15fe4211aa2615
Kyle Rich <kyle@blubracket.com>
2020-06-02 17:47:30 -0600

    Update some packages

--- .yadm/bin/os.sh
+++ .yadm/bin/os.sh
@@ -24,20 +24,6 @@ if command -v brew >/dev/null 2>&1; then
 fi
 
 
-install_ripgrep() {
-    RIPGREP_VERSION=11.0.1
-    RIPGREP=ripgrep-${RIPGREP_VERSION}-x86_64-unknown-linux-musl
-    RIPGREP_OPT=${HOME}/.local/opt/ripgrep
-
-    rm -rf ${RIPGREP_OPT}
-    mkdir -p ${RIPGREP_OPT}
-    curl -fsSL https://github.com/BurntSushi/ripgrep/releases/download/${RIPGREP_VERSION}/${RIPGREP}.tar.gz | tar xzC ${RIPGREP_OPT} --strip-components=1 ${RIPGREP}
-    ln -sf ${RIPGREP_OPT}/rg ${HOME}/.local/bin/rg
-    mkdir -p ${HOME}/.local/share/man/man1
-    ln -sf ${RIPGREP_OPT}/rg.1 ${HOME}/.local/share/man/man1/rg.1
-    mandb >/dev/null 2>&1
-}
-
 install_git() {
     $SUDO add-apt-repository -y ppa:git-core/ppa
     $SUDO apt-get update
@@ -50,7 +36,6 @@ install_git() {
 
 if command -v apt >/dev/null 2>&1; then
     $SUDO apt update && $SUDO apt install -y $(sed -e '/\s*#.*/d' ${HOME}/.config/packages/apt.txt)
-    install_ripgrep
     install_git
 fi
 
@@ -60,5 +45,4 @@ fi
 
 if command -v apk >/dev/null 2>&1; then
     $SUDO apk add --no-cache $(sed -e '/\s*#.*/d' ${HOME}/.config/packages/apk.txt)
-    install_ripgrep
 fi
